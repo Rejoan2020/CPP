dae4f8d3b0adfc35caa261ed7feac87b4caf0e2d
Rejoan2020 <md.rejoanur.rahman.apu@g.bracu.ac.bd>
2021-11-27 22:02:44 +0600

    Recursion of 01 knapsac transformed to DP

--- codeforce.cpp
+++ codeforce.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 long long t;
+int static ar[100][100];
+
 int knapsac(int wa[],int v[],int w,int n){
     if(w==0 || n==0) return 0;
-    if(wa[n-1]>w)return knapsac(wa,v,w,n-1);
+    if(ar[n][w]!=-1)return ar[n][w];
+
+    if(wa[n-1]>w)return ar[n][w]=knapsac(wa,v,w,n-1);
     else {
-        return max((v[n-1]+knapsac(wa,v,w-wa[n-1],n-1)),knapsac(wa,v,w,n-1));
+        return ar[n][w]=max((v[n-1]+knapsac(wa,v,w-wa[n-1],n-1)),knapsac(wa,v,w,n-1));
     }
 }
 int main() {
@@ -19,6 +23,11 @@ int main() {
         for(int i=0;i<n;i++){
             cin>>b[i];
         }
+        for(int i=0;i<100;i++){
+    for(int j=0;j<100;j++){
+        ar[i][j]=-1;
+    }
+}
         int ans=knapsac(a,b,10,n);
         cout<<ans<<endl;
     }
